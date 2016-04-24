e264a03c040a1375e49751a257a4d8240632f367
Mitch.Portier <Mitch.Portier@gmail.com>
2016-04-24 14:24:14 +0200

    Added breakpoint and bootstrap configuration to views handlers

--- views/footable-view.tpl.php
+++ views/footable-view.tpl.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Template to display a view as a table.
@@ -17,30 +18,36 @@
  * @ingroup views_templates
  */
 ?>
-<table <?php if ($classes): print 'class="'. $classes . '" '; endif; ?><?php print $attributes; ?>>
+<table
+  <?php foreach ($footable['attributes'] as $name => $value): ?>
+    <?php print $name . '="' . $value . '" '; ?>
+  <?php endforeach; ?>
+  <?php if ($classes): print 'class="' . $classes . '" '; endif; ?>
+  <?php print $attributes; ?>>
+
   <?php if (!empty($title)) : ?>
     <caption><?php print $title; ?></caption>
   <?php endif; ?>
   <?php if (!empty($header)) : ?>
     <thead>
-      <tr>
-        <?php foreach ($header as $field => $label): ?>
-          <th <?php if (!empty($data_toggle[$field])): print 'data-toggle="'. $data_toggle[$field] . '" '; endif; ?><?php if (!empty($data_hide[$field])): print 'data-hide="'. $data_hide[$field] . '" '; endif; ?><?php if ($header_classes[$field]): print 'class="'. $header_classes[$field] . '" '; endif; ?>>
-            <?php print $label; ?>
-          </th>
-        <?php endforeach; ?>
-      </tr>
+    <tr>
+      <?php foreach ($header as $field => $label): ?>
+        <th <?php if (!empty($data_toggle[$field])): print 'data-toggle="' . $data_toggle[$field] . '" '; endif; ?><?php if (!empty($data_breakpoints[$field])): print 'data-breakpoints="' . $data_breakpoints[$field] . '" '; endif; ?><?php if ($header_classes[$field]): print 'class="' . $header_classes[$field] . '" '; endif; ?>>
+          <?php print $label; ?>
+        </th>
+      <?php endforeach; ?>
+    </tr>
     </thead>
   <?php endif; ?>
   <tbody>
-    <?php foreach ($rows as $row_count => $row): ?>
-      <tr <?php if ($row_classes[$row_count]): print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  endif; ?>>
-        <?php foreach ($row as $field => $content): ?>
-          <td <?php if ($field_classes[$field][$row_count]): print 'class="'. $field_classes[$field][$row_count] . '" '; endif; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
-            <?php print $content; ?>
-          </td>
-        <?php endforeach; ?>
-      </tr>
-    <?php endforeach; ?>
+  <?php foreach ($rows as $row_count => $row): ?>
+    <tr <?php if ($row_classes[$row_count]): print 'class="' . implode(' ', $row_classes[$row_count]) . '"'; endif; ?>>
+      <?php foreach ($row as $field => $content): ?>
+        <td <?php if ($field_classes[$field][$row_count]): print 'class="' . $field_classes[$field][$row_count] . '" '; endif; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+          <?php print $content; ?>
+        </td>
+      <?php endforeach; ?>
+    </tr>
+  <?php endforeach; ?>
   </tbody>
 </table>
